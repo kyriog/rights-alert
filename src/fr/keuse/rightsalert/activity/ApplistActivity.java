dfd8b16ae0b05217839a493829ff2b41e3cb04c0
Cyril Glapa <cyril.glapa@keuse.fr>
2012-04-23 21:34:13 +0200

    Creating the project

---
+++ src/fr/keuse/rightsalert/activity/ApplistActivity.java
@@ -0,0 +1,14 @@
+package fr.keuse.rightsalert.activity;
+
+import fr.keuse.rightsalert.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ApplistActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.applist);
+    }
+}
